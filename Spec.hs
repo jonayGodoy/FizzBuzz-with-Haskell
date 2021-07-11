e64f1dcb1f6d8071c60287b82b0cad86620c9313
Jonay Godoy <jonay.godoy.reyes@gmail.com>
2021-07-11 13:21:14 +0100

    refactor rename

--- Spec.hs
+++ Spec.hs
@@ -1,20 +1,36 @@
 import Test.Hspec
 import Test.QuickCheck
 
-print1 = putStrLn "sss"
-
-print2 = (putStrLn "2222")
-
-print3 = putStrLn $ "333"
 
 isFizz :: Integer -> Bool
 isFizz x = x `mod` 3 == 0
 
-fizzGenerator :: Integer -> String
-fizzGenerator x
- | isFizz x = "Fizz"
- | otherwise = show x
+isBuzz :: Integer -> Bool
+isBuzz x = x `mod` 5 == 0
 
+fizzGenerator :: Integer -> FizzBuzzResult
+fizzGenerator x
+ | (isFizz x) && (isBuzz x) = FizzBuzz
+ | isFizz x = Fizz
+ | isBuzz x = Buzz
+ | otherwise = JustNumber x
+
+data FizzBuzzResult = FizzBuzz | Fizz | Buzz | JustNumber Integer deriving (Eq, Show)
+
+data FizzBuzzable' = FizzBuzz' | Fizz' Integer | Buzz' Integer | JustNumber' Integer deriving (Eq, Show)
+
+applyFizz :: FizzBuzzable' -> FizzBuzzable'
+applyFizz (Buzz' x) = if x `mod` 3 == 0 then FizzBuzz' else (Buzz' x)
+applyFizz (JustNumber' x) = if x `mod` 3 == 0 then (Fizz' x) else JustNumber' x
+applyFizz FizzBuzz' = FizzBuzz'
+
+applyBuzz :: FizzBuzzable' -> FizzBuzzable'
+applyBuzz (Fizz' x) = if x `mod` 5 == 0 then FizzBuzz' else (Fizz' x)
+applyBuzz (JustNumber' x) = if x `mod` 5 == 0 then (Buzz' x) else JustNumber' x
+applyBuzz FizzBuzz' = FizzBuzz'
+
+fizzGenerator' :: Integer -> FizzBuzzable'
+fizzGenerator' x = applyFizz . applyBuzz $ JustNumber' x
 
 main :: IO ()
 main = hspec $ do
@@ -23,7 +39,20 @@ main = hspec $ do
    True == True
  describe "FizzBuzz" $ do
   it "execute unfizzbuzzable number" $ do
-   fizzGenerator 1 == "1"
+   fizzGenerator 1 == JustNumber 1
   it "execute fizzable number" $ do
-   fizzGenerator 3 == "Fizz"
+   fizzGenerator 3 == Fizz
+  it "execute buzzable number" $ do
+   fizzGenerator 5 == Buzz
+  it "execute fizzbuzzable number" $ do
+   fizzGenerator 15 == FizzBuzz
+ describe "FizzBuzz2" $ do
+  it "execute return just number" $ do
+   fizzGenerator' 1 == JustNumber' 1
+  it "execute return fizz" $ do
+   fizzGenerator' 3 == Fizz' 3
+  it "execute return buzz" $ do
+   fizzGenerator' 5 == Buzz' 5
+  it "execute return buzz" $ do
+   fizzGenerator' 15 == FizzBuzz' 
 
